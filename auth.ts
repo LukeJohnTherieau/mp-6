3003e179a6ae7330db17274a01286858ef3330aa
Luke Therieau <luketherieau@outlook.com>
2026-04-19 09:03:40 -0400

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -2,13 +2,22 @@ import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
 
-export const {handlers, auth, signIn, signOut} = NextAuth(
+export const { handlers, auth, signIn, signOut } = NextAuth(
     {
         providers: [
             GitHubProvider(
                 {
-                    clientId: process.env.AUTH_GITHUB_ID ?? "", 
+                    clientId: process.env.AUTH_GITHUB_ID ?? "",
                     clientSecret: process.env.AUTH_GITHUB_SECRET ?? "",
+                    profile(profile) {
+                        return {
+                            id: profile.id.toString(),
+                            name: profile.name ?? profile.login,
+                            email: profile.email,
+                            image: profile.avatar_url,
+                            username: profile.login,
+                        }
+                    }
                 }
             )
         ],
